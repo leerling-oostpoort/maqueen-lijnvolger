80d387e81c4bbfb4fe78dbb602d9241fddb8c2c5
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-11-20 22:50:25 +0100

    Implementeer lijn volger algoritme

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+let right_line = 0
+let left_line = 0
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 25)
+let line_seen = 0
+basic.pause(1000)
+basic.forever(function () {
+    left_line = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+    right_line = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    if (right_line == 0 || left_line == 0) {
+        line_seen = 100
+    }
+    if (left_line == 0) {
+        if (right_line == 0) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 35)
+        }
+    } else {
+        if (line_seen > 0) {
+            line_seen += -1
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 35)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+        } else {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+        }
+    }
+})
